fe567005e57dc8120977a75963b5b295925263b0
JasonYANG17 <39414350+JasonYANG170@users.noreply.github.com>
2023-01-30 21:22:33 +0800

    添加项目文件。

---
+++ MFCApplication1/resource.h
@@ -0,0 +1,41 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCApplication1.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       136
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_BUTTON3                     1003
+#define IDC_BUTTON4                     1004
+#define IDC_BUTTON5                     1005
+#define IDC_BUTTON6                     1006
+#define IDC_BUTTON7                     1007
+#define IDC_BUTTON8                     1008
+#define IDC_BUTTON9                     1009
+#define IDC_BUTTON10                    1010
+#define IDC_BUTTON11                    1011
+#define IDC_BUTTON12                    1012
+#define IDC_BUTTON13                    1013
+#define IDC_BUTTON14                    1014
+#define IDC_BUTTON15                    1015
+#define IDC_BUTTON16                    1016
+#define IDC_BUTTON17                    1017
+#define IDC_EDIT2                       1018
+#define IDC_BUTTON18                    1019
+#define IDC_EDIT1                       1021
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
